8ba3e04a2e072339da8987ac8e1594502faf3b6f
Raghu Betina <raghu@firstdraft.com>
2018-06-21 20:51:12 +0000

    Direct association between courses and class_schedules

--- app/models/class_schedule.rb
+++ app/models/class_schedule.rb
@@ -1,6 +1,9 @@
 class ClassSchedule < ApplicationRecord
   # Direct associations
 
+  belongs_to :courses,
+             :class_name => "Course"
+
   # Indirect associations
 
   # Validations
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,10 @@
 class Course < ApplicationRecord
   # Direct associations
 
+  has_many   :class_schedules,
+             :foreign_key => "courses_id",
+             :dependent => :nullify
+
   has_many   :course_materials,
              :dependent => :nullify
 
