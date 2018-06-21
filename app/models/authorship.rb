1244681b8358e30e3f3e3292bbc97c6625578576
Raghu Betina <raghu@firstdraft.com>
2018-06-21 20:51:12 +0000

    Direct association between books and authorships

--- app/models/authorship.rb
+++ app/models/authorship.rb
@@ -1,6 +1,9 @@
 class Authorship < ApplicationRecord
   # Direct associations
 
+  belongs_to :books,
+             :class_name => "Book"
+
   belongs_to :authors,
              :class_name => "Author"
 
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,10 @@
 class Book < ApplicationRecord
   # Direct associations
 
+  has_many   :authorships,
+             :foreign_key => "books_id",
+             :dependent => :destroy
+
   has_many   :course_materials,
              :dependent => :nullify
 
