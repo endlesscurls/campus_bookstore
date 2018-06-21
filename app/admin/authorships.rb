bdfc7c0c1ac869c31872ff39bf9a9ea0e449c935
Raghu Betina <raghu@firstdraft.com>
2018-06-21 20:51:45 +0000

    Whitelist columns in Authorship for ActiveAdmin

--- app/admin/authorships.rb
+++ app/admin/authorships.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Authorship do
+
+ permit_params :books_id, :authors_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
