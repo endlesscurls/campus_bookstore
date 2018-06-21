4177af2c8721de604bcf179019673c1cc626b4cc
Raghu Betina <raghu@firstdraft.com>
2018-06-21 20:50:35 +0000

    Generate authors resource with starter:resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  # Routes for the Author resource:
+  # CREATE
+  get "/authors/new", :controller => "authors", :action => "new"
+  post "/create_author", :controller => "authors", :action => "create"
+
+  # READ
+  get "/authors", :controller => "authors", :action => "index"
+  get "/authors/:id", :controller => "authors", :action => "show"
+
+  # UPDATE
+  get "/authors/:id/edit", :controller => "authors", :action => "edit"
+  post "/update_author/:id", :controller => "authors", :action => "update"
+
+  # DELETE
+  get "/delete_author/:id", :controller => "authors", :action => "destroy"
+  #------------------------------
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
