e6094afa5983d1a16f455a67f10a21d59d32a464
Raghu Betina <raghu@firstdraft.com>
2018-06-21 20:51:49 +0000

    Whitelist columns in ClassSchedule for ActiveAdmin

--- app/admin/class_schedules.rb
+++ app/admin/class_schedules.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register ClassSchedule do
+
+ permit_params :courses_id, :students_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
