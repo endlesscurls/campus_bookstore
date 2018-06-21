7c3ae8d35359b9b1c86b0d84eb50bf267ad11979
Raghu Betina <raghu@firstdraft.com>
2018-06-21 20:51:22 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "authors#index"
   # Routes for the Class_schedule resource:
   # CREATE
